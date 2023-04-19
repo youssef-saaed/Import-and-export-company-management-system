6278fc0efce18b0343ed0c353d5c05193dd54d87
Ashraf <aadhameltholth@gmail.com>
2023-04-19 23:20:26 +0200

    Hell world

---
+++ Import and export company management system/Person.h
@@ -0,0 +1,54 @@
+//
+// Created by Adham Ashraf on 4/19/2023.
+//
+
+
+#pragma once
+
+#include "Date.h"
+#include "Account.h"
+
+class Person {
+private:
+    string name;
+    Date birthdate;
+    string address;
+    int phonenum;
+    int gender;
+    string profilePic;
+    int referecode;
+    string membership;
+    Account account;
+
+public:
+    Person();
+    string getName();
+    void setName(string name);
+
+    int getAge();
+    Date getBirthdate();
+    void setBirthdate(Date birthdate);
+
+    string getAddress();
+    void setAddress(string address);
+
+    int getPhonenum();
+
+    int getGender();
+    void setGender(int gender);
+
+    int getReferecode();
+    void setReferecode(int referecode);
+
+    string getProfilePic();
+    void setProfilePic(string profilePic);
+
+    string getMemberShip();
+    void setMemberShip(string membership);
+
+    void viewInfo();
+    void delete_account();
+};
+
+
+#ndif //IMPORT_AND_EXPORT_COMPANY_MANAGEMENT_SYSTEM_PERSON_H
