b53cffe7f9b9ab37867a86cc5ab9afe489e4f9c3
Ashraf <aadhameltholth@gmail.com>
2023-04-19 23:49:39 +0200

    Hell world 4

---
+++ Import and export company management system/Accounts.h
@@ -0,0 +1,33 @@
+//
+// Created by Ashraf on 4/19/2023.
+//
+
+#pragma once
+
+class Employee {
+private:
+    int EmployeeID;
+    string jobTitle;
+    float salary;
+    string department;
+    float overtimehrs;
+
+public:
+    Employee();
+
+    int getemployeeID();
+    void setemployeeID(int EmployeeID);
+
+    string getDepartment();
+    void setDepartment(string department);
+
+    float getSalary();
+    void setSalary(float salary);
+
+    string getJobTitle();
+    void setJobTitle(string jobTitle);
+
+    float overTime_Ecalc();
+};
+
+#endif //IMPORT_AND_EXPORT_COMPANY_MANAGEMENT_SYSTEM_ACCOUNTS_H
