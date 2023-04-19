c5ce3ae629548a97a392e8ac12caf3af8b79a0bd
Mohamed-Mohamed-Hozien <s-mohamed.hozien@zewailcity.edu.eg>
2023-04-19 23:56:25 +0200

    Hozien prototype classs for products

--- Import and export company management system/Product.h
+++ Import and export company management system/Product.h
@@ -1,5 +1,46 @@
 #pragma once
+#include <string>
+
+
 class Product
 {
+private:
+	std::string lifeCyclePeriod, name, description, image;
+	double price, amount, mass, volume;
+	int productID;
+puplic:
+	Product(const std::string&, const std::string&, const std::string&, const std::string&, const double&, const double&, const double&, const double&, const int&);
+	~Product();
+
+	void setlifeCyclePeriod(const std::string&);
+	std::string getlifeCyclePeriod() const;
+
+	void setName(const std::string&);
+	std::string getName() const;
+
+	void setDescription(const std::string&);
+	std::string getDescription() const;
+
+	void setImage(const std::string&);
+	std::string getImage() const;
+
+	void setPrice(const double&);
+	double getPrice() const;
+
+	void setAmount(const double&);
+	double getAmount() const;
+
+	void setMass(const double&);
+	double getMass() const;
+
+	void setVolume(const double&);
+	double getVolume() const;
+
+	void setProductID(const int&);
+	int getProductID() const;
+
+	bool isExists(int& productID);
+
+	void editProduct()
 };
 
