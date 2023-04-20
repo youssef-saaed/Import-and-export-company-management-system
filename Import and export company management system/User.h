c5220404364fc0de82bc68a79d98737966b37422
aadhamashraf <119948679+aadhamashraf@users.noreply.github.com>
2023-04-20 16:25:11 +0200

    Update User.h

--- Import and export company management system/User.h
+++ Import and export company management system/User.h
@@ -11,11 +11,17 @@ private:
 
 public:
     User(std::string name, Date birthdate, std::string address, int phonenum, int gender, std::string profilePic, int referecode, std::string membership, Account account, double Ubalance, Cart userCart, Order pastOrders);
+    
     double getBalance() const;
     bool setBalance(double const& Ubalance);
+    
     void viewProfile() const;
+    
     void view_P_history() const;
+    
     void change_Password() const;
+    
     void viewInfo() const override;
+    
     void delete_account() const override;
 };
