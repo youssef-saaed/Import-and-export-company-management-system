0cc9ff8c0ce28f0ddccbaf2fecf256750c5c8e4a
aadhamashraf <119948679+aadhamashraf@users.noreply.github.com>
2023-04-20 12:21:06 +0200

    Update User.h

--- Import and export company management system/User.h
+++ Import and export company management system/User.h
@@ -1,23 +1,21 @@
 #pragma once
 #include "Cart.h"
 #include "Order.h"
+#include "Person.h"
 
-class User {
+class User : public Person {
 private:
     double Ubalance;
     Cart userCart;
     Order pastOrders;
 
 public:
-   User(float Ubalance, Cart userCart, Order pastOrders);
-
+    User(std::string name, Date birthdate, std::string address, int phonenum, int gender, std::string profilePic, int referecode, std::string membership, Account account, double Ubalance, Cart userCart, Order pastOrders);
     double getBalance() const;
-    void setBalance(double const& Ubalance);
-
+    bool setBalance(double const& Ubalance);
     void viewProfile() const;
     void view_P_history() const;
-
     void change_Password() const;
+    void viewInfo() const override;
+    void delete_account() const override;
 };
-
-#endif // IMPORT_AND_EXPORT_COMPANY_MANAGEMENT_SYSTEM_USER_H
