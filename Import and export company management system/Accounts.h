bce27aa1345298eb37a2c3c81e89eae398eb9029
aadhamashraf <119948679+aadhamashraf@users.noreply.github.com>
2023-04-20 16:22:42 +0200

    Update Accounts.h

--- Import and export company management system/Accounts.h
+++ Import and export company management system/Accounts.h
@@ -13,16 +13,22 @@ private:
 
 public:   
     Accounts(std::string const& username, std::string const& password, std::string const& email, bool const& isverified, std::string const& accountType);
+    
     std::string const& getUsername() const;
     bool setUsername(std::string const& username);
+    
     std::string const& getPassword() const;
     bool setPassword(std::string const& password);
+    
     std::string const& getEmail() const;
     bool setEmail(std::string const& email);
+    
     bool const& getIsVerified() const;
     bool setIsVerified(bool const& isverified);
+    
     std::string const& getAccountType() const;
     bool setAccountType(std::string const& accountType);
+    
     void verify() const;
 };
 
