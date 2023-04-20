e37cfa19ceaa9d6c512fb521556c2e77d051afac
aadhamashraf <119948679+aadhamashraf@users.noreply.github.com>
2023-04-20 16:24:25 +0200

    Update Person.h

--- Import and export company management system/Person.h
+++ Import and export company management system/Person.h
@@ -18,22 +18,31 @@ protected:
 public:
     Person(std::string name, Date birthdate, std::string address, int phonenum, int gender, std::string profilePic, int referecode, std::string membership, Account account);
     virtual ~Person() {};
+    
     std::string getName() const;
     bool setName(std::string const& name);
+     
     int getAge() const;
     Date getBirthdate() const;
     bool setBirthdate(Date const& birthdate);
+    
     std::string getAddress() const;
     bool setAddress(std::string const& address);
+    
     int getPhonenum() const;
+    
     int getGender() const;
     bool setGender(int const& gender);
+    
     int getReferecode() const;
     bool setReferecode(int const& referecode);
+    
     std::string getProfilePic() const;
     bool setProfilePic(std::string const& profilePic);
+    
     std::string getMemberShip() const;
     bool setMemberShip(std::string const& membership);
+    
     virtual void viewInfo() const = 0;
     virtual void delete_account() const = 0;
 };
