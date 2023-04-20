f5e42700e23c76b9b781cf0c65b46777d5250d0b
aadhamashraf <119948679+aadhamashraf@users.noreply.github.com>
2023-04-20 16:23:30 +0200

    Update Employee.h

--- Import and export company management system/Employee.h
+++ Import and export company management system/Employee.h
@@ -12,14 +12,19 @@ private:
 
 public:
     Employee(std::string name, Date birthdate, std::string address, int phonenum, int gender, std::string profilePic, int referecode, std::string membership, Account account, int EmployeeID, std::string jobTitle, double salary, std::string department, double overtimehrs);
+    
     int getemployeeID() const;
     bool setemployeeID(int const& EmployeeID);
+    
     std::string getDepartment() const;
     bool setDepartment(std::string const& department);
+    
     double getSalary() const;
     bool setSalary(double const& salary);
+    
     std::string getJobTitle() const;
     bool setJobTitle(std::string const& jobTitle);
+    
     double overTime_Ecalc() const;
     void viewInfo() const override;
     void delete_account() const override;
