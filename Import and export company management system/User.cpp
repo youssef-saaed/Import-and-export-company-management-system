6bd74dd59c4db3cefe3914dfe4386cb4d09ebd34
Ashraf <aadhameltholth@gmail.com>
2023-04-25 16:14:19 +0200

    بتاعي

--- Import and export company management system/User.cpp
+++ Import and export company management system/User.cpp
@@ -1,9 +1,45 @@
 #include "User.h"
 
+
+User::User(std::string name, Date birthdate, std::string address, int phonenum, int gender, std::string profilePic,
+    int referecode, std::string membership, Account account, double Ubalance, Cart userCart, Order* pastOrders)
+    : Person(name, birthdate, address, phonenum, gender, profilePic, referecode, membership, account),
+    Ubalance(Ubalance), userCart(userCart), pastOrders(pastOrders) {}
+
+User::User() : Ubalance(0.0), userCart(), pastOrders(0) {}
+
+double User::getBalance() const
+{
+    return Ubalance;
+}
+
+bool User::setBalance(double const& Ubalance)
+{
+    this->Ubalance = Ubalance;
+    return true;
+}
+
+void User::viewProfile() const
+{
+
+}
+
+void User::view_P_history() const
+{
+
+}
+
+void User::change_Password() const
+{
+
+}
+
 void User::viewInfo() const
 {
+
 }
 
 void User::delete_account() const
 {
+
 }
--- Import and export company management system/User.h
+++ Import and export company management system/User.h
@@ -1,28 +1,28 @@
-#pragma once
 #include "Cart.h"
 #include "Order.h"
 #include "Person.h"
 
 class User : public Person {
+
 private:
     double Ubalance;
     Cart userCart;
-    Order *pastOrders;
+    Order* pastOrders;
 
 public:
     User();
     User(std::string name, Date birthdate, std::string address, int phonenum, int gender, std::string profilePic, int referecode, std::string membership, Account account, double Ubalance, Cart userCart, Order* pastOrders);
-    
+
     double getBalance() const;
     bool setBalance(double const& Ubalance);
-    
+
     void viewProfile() const;
-    
+
     void view_P_history() const;
-    
+
     void change_Password() const;
-    
+
     void viewInfo() const override;
-    
+
     void delete_account() const override;
-};
+};)
