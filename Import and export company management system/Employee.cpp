8bd3f2892532e1f991ac80627068e449d6dcb9c3
Ashraf <aadhameltholth@gmail.com>
2023-04-25 16:16:18 +0200

    العو جه

---
+++ Import and export company management system/Employee.cpp
@@ -0,0 +1,67 @@
+#include "Employee.h"
+
+Employee::Employee(std::string name, Date birthdate, std::string address, int phonenum, int gender, std::string profilePic, int referecode, std::string membership, Account account, int EmployeeID, std::string jobTitle, double salary, std::string department, double overtimehrs)
+    : Person(name, birthdate, address, phonenum, gender, profilePic, referecode, membership, account), EmployeeID(EmployeeID), jobTitle(jobTitle), salary(salary), department(department), overtimehrs(overtimehrs) {}
+
+int Employee::getemployeeID() const
+{
+    return EmployeeID;
+}
+
+bool Employee::setemployeeID(int const& EmployeeID)
+{
+    this->EmployeeID = EmployeeID;
+    return true;
+}
+
+
+std::string Employee::getDepartment() const
+{
+    return department;
+}
+
+bool Employee::setDepartment(std::string const& department)
+{
+    this->department = department;
+    return true;
+}
+
+
+double Employee::getSalary() const {
+
+    return salary;
+}
+
+bool Employee::setSalary(double const& salary)
+{
+    this->salary = salary;
+    return true;
+}
+
+
+std::string Employee::getJobTitle() const
+{
+    return jobTitle;
+}
+
+bool Employee::setJobTitle(std::string const& jobTitle)
+{
+    this->jobTitle = jobTitle;
+    return true;
+}
+
+
+double Employee::overTime_Ecalc() const
+{
+
+}
+
+void Employee::viewInfo() const
+{
+
+}
+
+void Employee::delete_account() const
+{
+
+}
--- Import and export company management system/Employee.h
+++ Import and export company management system/Employee.h
@@ -3,6 +3,7 @@
 #include "Person.h"
 
 class Employee : public Person {
+
 private:
     int EmployeeID;
     std::string jobTitle;
@@ -12,19 +13,19 @@ private:
 
 public:
     Employee(std::string name, Date birthdate, std::string address, int phonenum, int gender, std::string profilePic, int referecode, std::string membership, Account account, int EmployeeID, std::string jobTitle, double salary, std::string department, double overtimehrs);
-    
+
     int getemployeeID() const;
     bool setemployeeID(int const& EmployeeID);
-    
+
     std::string getDepartment() const;
     bool setDepartment(std::string const& department);
-    
+
     double getSalary() const;
     bool setSalary(double const& salary);
-    
+
     std::string getJobTitle() const;
     bool setJobTitle(std::string const& jobTitle);
-    
+
     double overTime_Ecalc() const;
     void viewInfo() const override;
     void delete_account() const override;
