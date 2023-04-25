00cd7a37b99af1961c09df8eb54c5677adc447b8
Ashraf <aadhameltholth@gmail.com>
2023-04-25 16:11:22 +0200

    بحبك

--- Import and export company management system/Person.cpp
+++ Import and export company management system/Person.cpp
@@ -1,64 +1,46 @@
 #include "Person.h"
 
-// Constructor
 Person::Person(std::string name, Date birthdate, std::string address, int phonenum, int gender, std::string profilePic, int referecode, std::string membership, Account account)
     : name(name), birthdate(birthdate), address(address), phonenum(phonenum), gender(gender), profilePic(profilePic), referecode(referecode), membership(membership), account(account) {}
 
-// Getters
-std::string Person::getName() const {
+Person::~Person()
+{
+}
+
+std::string Person::getName() const
+{
     return name;
 }
 
-int Person::getAge() const {
-    return birthdate.getAge();
-}
-
-Date Person::getBirthdate() const {
-    return birthdate;
-}
-
-std::string Person::getAddress() const {
-    return address;
-}
-
-int Person::getPhonenum() const {
-    return phonenum;
-}
-
-int Person::getGender() const {
-    return gender;
-}
-
-int Person::getReferecode() const {
-    return referecode;
-}
-
-std::string Person::getProfilePic() const {
-    return profilePic;
-}
-
-std::string Person::getMemberShip() const {
-    return membership;
-}
-
-// Setters
-bool Person::setName(std::string const& name) {
-    if (!name.empty()) {
+bool Person::setName(std::string const& name)
+{
+    if (!name.empty())
+    {
         this->name = name;
         return true;
     }
     return false;
 }
 
-bool Person::setBirthdate(Date const& birthdate) {
-    if (birthdate.isValid()) {
-        this->birthdate = birthdate;
-        return true;
-    }
-    return false;
+
+Date Person::getBirthdate() const
+{
+    return birthdate;
 }
 
-bool Person::setAddress(std::string const& address) {
+int Person::getAge() const
+{
+    return birthdate.getAge();
+}
+
+
+std::string Person::getAddress() const
+{
+    return address;
+}
+
+bool Person::setAddress(std::string const& address)
+{
     if (!address.empty()) {
         this->address = address;
         return true;
@@ -66,7 +48,26 @@ bool Person::setAddress(std::string const& address) {
     return false;
 }
 
-bool Person::setGender(int const& gender) {
+
+int Person::getPhonenum() const
+{
+    return phonenum;
+}
+
+bool Person::setPhonenum(int const& phonenum)
+{
+
+
+}
+
+
+int Person::getGender() const
+{
+    return gender;
+}
+
+bool Person::setGender(int const& gender)
+{
     if (gender == 0 || gender == 1) {
         this->gender = gender;
         return true;
@@ -74,26 +75,70 @@ bool Person::setGender(int const& gender) {
     return false;
 }
 
-bool Person::setReferecode(int const& referecode) {
-    if (referecode >= 0) {
+
+int Person::getReferecode() const
+{
+    return referecode;
+}
+
+bool Person::setReferecode(int const& referecode)
+{
+    if (referecode >= 0)
+    {
         this->referecode = referecode;
         return true;
     }
     return false;
 }
 
-bool Person::setProfilePic(std::string const& profilePic) {
-    if (!profilePic.empty()) {
+
+std::string Person::getProfilePic() const
+{
+    return profilePic;
+}
+
+bool Person::setProfilePic(std::string const& profilePic)
+{
+    if (!profilePic.empty())
+    {
         this->profilePic = profilePic;
         return true;
     }
     return false;
 }
 
-bool Person::setMemberShip(std::string const& membership) {
-    if (!membership.empty()) {
+std::string Person::getMembership() const
+{
+    return std::string();
+}
+
+
+
+std::string Person::getMemberShip() const
+{
+    return membership;
+}
+bool Person::setMemberShip(std::string const& membership)
+{
+    if (!membership.empty())
+    {
         this->membership = membership;
         return true;
     }
     return false;
 }
+
+bool Person::setMembership(std::string const& membership)
+{
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
--- Import and export company management system/Person.h
+++ Import and export company management system/Person.h
@@ -4,47 +4,48 @@
 #include "Date.h"
 #include "Account.h"
 
-class Person{
-        protected:
-        std::string name;
-        int phonenum;
-        int gender;
-        Date birthdate;
-        std::string address;
-        std::string profilePic;
-        std::string membership;
-        Account account;
-        int referecode;
+class Person {
 
-        public:
-        Person(std::string name, Date birthdate, std::string address, int phonenum, int gender, std::string profilePic, int referecode, std::string membership, Account account);
-        virtual ~Person();
+protected:
+    std::string name;
+    int phonenum;
+    int gender;
+    Date birthdate;
+    std::string address;
+    std::string profilePic;
+    std::string membership;
+    Account account;
+    int referecode;
 
-        std::string getName() const;
-        bool setName(std::string const& name);
+public:
+    Person(std::string name, Date birthdate, std::string address, int phonenum, int gender, std::string profilePic, int referecode, std::string membership, Account account);
+    virtual ~Person();
 
-        int getPhonenum() const;
-        bool setPhonenum(int const& phonenum);
+    std::string getName() const;
+    bool setName(std::string const& name);
 
-        int getGender() const;
-        bool setGender(int const& gender);
+    int getPhonenum() const;
+    bool setPhonenum(int const& phonenum);
 
-        int getAge() const;
-        Date getBirthdate() const;
-        bool setBirthdate(Date const& birthdate);
+    int getGender() const;
+    bool setGender(int const& gender);
 
-        std::string getAddress() const;
-        bool setAddress(std::string const& address);
+    int getAge() const;
+    Date getBirthdate() const;
+    bool setBirthdate(Date const& birthdate);
 
-        std::string getProfilePic() const;
-        bool setProfilePic(std::string const& profilePic);
+    std::string getAddress() const;
+    bool setAddress(std::string const& address);
 
-        std::string getMembership() const;
-        bool setMembership(std::string const& membership);
+    std::string getProfilePic() const;
+    bool setProfilePic(std::string const& profilePic);
 
-        int getReferecode() const;
-        bool setReferecode(int const& referecode);
+    std::string getMembership() const;
+    bool setMembership(std::string const& membership);
 
-        virtual void viewInfo() const = 0;
-        virtual void delete_account() = 0;
+    int getReferecode() const;
+    bool setReferecode(int const& referecode);
+
+    virtual void viewInfo() const = 0;
+    virtual void delete_account() = 0;
 };
