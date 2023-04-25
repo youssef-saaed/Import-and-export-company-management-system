e45ed3a4a2b0bd40c3ea0e0ddce8d104e30e959c
Ashraf <aadhameltholth@gmail.com>
2023-04-25 15:51:19 +0200

    .

---
+++ Import and export company management system/Account.cpp
@@ -0,0 +1,79 @@
+
+#include "Account.h"
+#include <string>
+Account::Account(std::string const& username, std::string const& password, std::string const& email, bool const& isverified,
+    std::string const& accountType) {
+    Account::username = username;
+    Account::password = password;
+    Account::email = email;
+    Account::isverified = isverified;
+    Account::accountType = accountType;
+}
+
+Account::Account()
+{
+    username = "";
+    password = "";
+    email = "";
+    isverified = false;
+    accountType = "";
+}
+
+std::string const& Account::getUsername() const
+{
+    return username;
+}
+
+bool Account::setUsername(std::string const& username)
+{
+    Account::username = username;
+    return true;
+}
+
+std::string const& Account::getPassword() const
+{
+    return password;
+}
+
+bool Account::setPassword(std::string const& password)
+{
+    Account::password = password;
+    return true;
+}
+
+std::string const& Account::getEmail() const
+{
+    return email;
+}
+
+bool Account::setEmail(std::string const& email)
+{
+    Account::email = email;
+    return true;
+}
+
+bool const& Account::getIsVerified() const
+{
+    return isverified;
+}
+
+bool Account::setIsVerified(bool const& isverified)
+{
+    Account::isverified = isverified;
+    return true;
+}
+
+std::string const& Account::getAccountType() const
+{
+    return accountType;
+}
+
+bool Account::setAccountType(std::string const& accountType)
+{
+    Account::accountType = accountType;
+    return true;
+}
+
+void Account::verify() const {
+
+}
--- Import and export company management system/Account.h
+++ Import and export company management system/Account.h
@@ -1,7 +1,8 @@
 #pragma once
-#include <string>
+
 
 class Account {
+
 private:
     std::string username;
     std::string password;
@@ -9,24 +10,24 @@ private:
     bool isverified;
     std::string accountType;
 
-public:   
+public:
     Account(std::string const& username, std::string const& password, std::string const& email, bool const& isverified, std::string const& accountType);
     Account();
-    
+
     std::string const& getUsername() const;
     bool setUsername(std::string const& username);
-    
+
     std::string const& getPassword() const;
     bool setPassword(std::string const& password);
-    
+
     std::string const& getEmail() const;
     bool setEmail(std::string const& email);
-    
+
     bool const& getIsVerified() const;
     bool setIsVerified(bool const& isverified);
-    
+
     std::string const& getAccountType() const;
     bool setAccountType(std::string const& accountType);
-    
+
     void verify() const;
 };
