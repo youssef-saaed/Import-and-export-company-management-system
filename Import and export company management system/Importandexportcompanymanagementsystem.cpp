844cd9a9018efe7e6cc66c57a936db63205cbadc
Ashraf <aadhameltholth@gmail.com>
2023-05-01 16:07:03 +0200

    .

--- Import and export company management system/Importandexportcompanymanagementsystem.cpp
+++ Import and export company management system/Importandexportcompanymanagementsystem.cpp
@@ -1,13 +1,32 @@
 #include "Importandexportcompanymanagementsystem.h"
 
-Importandexportcompanymanagementsystem::Importandexportcompanymanagementsystem(QWidget *parent)
+Importandexportcompanymanagementsystem::Importandexportcompanymanagementsystem(QWidget* parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
     ui.passwordI->setEchoMode(QLineEdit::Password);
     ui.passwordLoginI->setEchoMode(QLineEdit::Password);
-    /*QObject::connect(ui.registerBtn,Q)*/
+
+    connect(ui.registerBtn, &QPushButton::clicked, this, &Importandexportcompanymanagementsystem::registerUser);
 }
 
 Importandexportcompanymanagementsystem::~Importandexportcompanymanagementsystem()
 {}
+
+void Importandexportcompanymanagementsystem::loginUser()
+{
+    QString UserName = ui.userLoginI->text();
+    QString password = ui.passwordLoginI->text();
+    
+}
+
+void Importandexportcompanymanagementsystem::registerUser()
+{
+    QString name = ui.nameI->text();
+    QString username = ui.userI->text();
+    QString email = ui.emailI->text();
+    QString password = ui.passwordI->text();
+    QString phone = ui.phoneNumI->text();
+    QString address = ui.addressI->text();
+
+}
--- Import and export company management system/Importandexportcompanymanagementsystem.h
+++ Import and export company management system/Importandexportcompanymanagementsystem.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <QMessageBox>
 #include "ui_Importandexportcompanymanagementsystem.h"
 
 class Importandexportcompanymanagementsystem : public QMainWindow
@@ -8,9 +9,12 @@ class Importandexportcompanymanagementsystem : public QMainWindow
     Q_OBJECT
 
 public:
-    Importandexportcompanymanagementsystem(QWidget *parent = nullptr);
+    Importandexportcompanymanagementsystem(QWidget* parent = nullptr);
     ~Importandexportcompanymanagementsystem();
 
+private slots:
+    void registerUser();
+    void loginUser();
 private:
     Ui::ImportandexportcompanymanagementsystemClass ui;
 };
