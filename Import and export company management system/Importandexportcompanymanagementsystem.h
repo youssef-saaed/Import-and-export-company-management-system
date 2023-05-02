cb865b8c13e5a523a81ec1f77a8252ea6e9ae621
Mohamed-Mohamed-Hozien <s-mohamed.hozien@zewailcity.edu.eg>
2023-05-02 14:45:22 +0300

    .

--- Import and export company management system/Importandexportcompanymanagementsystem.h
+++ Import and export company management system/Importandexportcompanymanagementsystem.h
@@ -9,7 +9,16 @@ class Importandexportcompanymanagementsystem : public QMainWindow
     Q_OBJECT
 
 public:
-    Importandexportcompanymanagementsystem(QWidget* parent = nullptr);
+    Importandexportcompanymanagementsystem(QWidget* parent = nullptr)
+        : QMainWindow(parent)
+    {
+        ui.setupUi(this);
+        ui.passwordI->setEchoMode(QLineEdit::Password);
+        ui.passwordLoginI->setEchoMode(QLineEdit::Password);
+
+        connect(ui.registerBtn, &QPushButton::clicked, this, &Importandexportcompanymanagementsystem::registerUser);
+        connect(ui.loginBtn, &QPushButton::clicked, this, &Importandexportcompanymanagementsystem::loginUser);
+    }
     ~Importandexportcompanymanagementsystem();
 
 private slots:
