f8238fbaaf978cfa8facaf0b03ffe530eede53db
Mohamed-Mohamed-Hozien <s-mohamed.hozien@zewailcity.edu.eg>
2023-05-06 18:36:06 +0300

    come eat it

--- Import and export company management system/User.cpp
+++ Import and export company management system/User.cpp
@@ -1,5 +1,7 @@
 #include "User.h"
-
+#include <fstream>
+#include <sstream>
+#include <vector>
 User::User(std::string name, Date birthdate, std::string address, std::string phonenum, Gender gender, std::string profilePic, std::string membership, Account account)
     : Person(name, birthdate, address, phonenum, gender, profilePic, account),membership(membership) {}
 
@@ -51,8 +53,76 @@ std::string User::Register()
     if (account.getEmail() == "") {
         return "email is empty, please enter an email";
     }
+    if (phonenum == "") {
+        return "Phone Number is empty, please enter a Phone Number";
+    }
+    if (address == "") {
+        return "address is empty, please enter an address";
+    }
 
-    if ()
 
-    return std::string();
+    std::ifstream handler;
+    std::string path = "youssef-saaed\\Import-and-export-company-management-system\\Import and export company management system\\DB\\userAndEmployeeData.csv";
+    handler.open(path);
+    std::string line = "";
+
+
+    if (handler) {
+        bool all_data_found = true;
+        int row_count = 0;
+        bool data_found;
+        while (std::getline(handler, line)) {
+            row_count++;
+
+            std::vector <std::string> data;
+            std::string user;
+            std::istringstream ss(line);
+            while (std::getline(ss, user, ',')) {
+                data.push_back(user);
+            }
+
+            std::string username = account.getUsername();
+            std::string email = account.getEmail();
+            std::string password = account.getPassword();
+            std::string phoneNumber = phonenum;
+
+            std::vector<std::pair<std::string, std::string>> required_data = {
+               {"name", name},
+               {"username", username},
+               {"email", email},
+               {"password", password},
+               {"phonenumber", phoneNumber},
+               {"address", address}
+            };
+            for (auto required : required_data) {
+                bool exists = false;
+                for (size_t i = 0; i < data.size(); i++) {
+                    if (data[i] == required.second) {
+                        exists = true;
+                        break;
+                    }
+                }
+                if (!exists) {
+                    std::cout << "Data '" << required.first << "' missing in row " << row_count << std::endl;
+                    all_data_found = false;
+                    data_found = false;
+                }
+            }
+
+            if (data_found) {
+                std::cout << "All required data found in row " << row_count << std::endl;
+            }
+        }
+
+        if (all_data_found) {
+            std::cout << "All required data found in all rows!" << std::endl;
+        }
+    }
+    else {
+        std::cout << "File not found: " << path << std::endl;
+    }
+
+
+        return std::string();
+    }
 }
